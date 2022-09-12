95f05fe20b529cff149d5ed0fa32546fd318e9d3
Samir Innovation <113102510+samirdave1261@users.noreply.github.com>
2022-09-12 18:23:31 +0530

    Add files via upload

---
+++ Basic Of C#/ConsoleApp11/ConsoleApp11/ClsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class ClsTest
+    {
+        private string _firstName { get; set; }
+        public string _LastName { get; set; }
+
+        public string _firstProp
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+
+        public ClsTest() 
+        {
+            _firstProp = "Viks ";
+            
+        }
+
+        public ClsTest(string _data)
+        {
+            Console.WriteLine("COnstructor...");
+            _firstName = _data;
+        }
+
+        ~ ClsTest()
+        {
+            Console.WriteLine("Destructor...");
+        }
+
+    }
+}
---
+++ Basic Of C#/ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public class Program : AccessMOdifier
+    {
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                List<string> data = new List<string>();
+                data.Add("dsds");
+                data.Add("ssfsfv");
+                data.Add("dss");
+
+                Console.WriteLine(data.Count);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally{ Console.WriteLine("Finally!"); }
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+    public class AccessMOdifier
+    {
+        protected void sum(int a)
+        {
+            Console.WriteLine(a);
+        }
+
+        public void summm()
+        {
+            sum(12);
+        }
+    }
+
+
+    //Interface
+    public interface _interfaceOne
+    {
+        void _classoneMethod();
+    }
+
+    public interface _interfaceTwo
+    {
+        void _classoneMethod();
+    }
+    public class _clsOne : _interfaceOne, _interfaceTwo
+    {
+        public void _classoneMethod()
+        { Console.WriteLine("1"); }
+    }
+
+    public class _classTwo
+    {
+        public void _classTwoMethod()
+        { Console.WriteLine("2"); }
+    }
+
+
+
+    public abstract class _Abstract1
+    {
+        public int a = 2;
+        public abstract void _method();
+    }
+
+    public class _abstract2 : _Abstract1
+    {
+        public override void _method()
+        {
+            Console.WriteLine("Abstraction 1...");
+        }
+    }
+
+    public class _abstract3 : _Abstract1
+    {
+        public override void _method()
+        {
+            Console.WriteLine("Abstraction 2...");
+        }
+    }
+
+    public class _ParentCls
+    {
+        public string _FirstName;
+
+        public virtual void _ghh() { Console.WriteLine("Parent Class"); }
+    }
+
+    public class _ChildCls : _ParentCls
+    {
+        public string _lastName;
+        public override void _ghh() { Console.WriteLine("Base Class"); }
+    }
+
+    static class StaticCls
+    {
+        public static string _var;
+    }
+
+    class DestructorDemo
+    {
+        public DestructorDemo()
+        {
+            Console.WriteLine("Constructor Object Created");
+        }
+        ~DestructorDemo()
+        {
+            string type = GetType().Name;
+            Console.WriteLine($"Object {type} is Destroyed");
+        }
+
+    }
+
+    class ClsTestDemo
+    {
+        public void _clsmethod(int val) { val += val; Console.WriteLine(val); }
+
+        ~ClsTestDemo() { Console.WriteLine("sfzssfbv"); }
+
+    }
+}
